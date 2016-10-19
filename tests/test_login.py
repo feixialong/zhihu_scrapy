ef45e8487914760bf04c8f302c722fa280dd6f01
Shun <wumingshun@163.com>
2016-10-19 17:52:54 +0800

    完成登录

--- tests/test_login.py
+++ tests/test_login.py
@@ -6,7 +6,7 @@ from zhihu_scrapy.spiders import login
 
 class TestLogIn(unittest.TestCase):
     def setUp(self):
-        pass
+        self.test_login = login.Login()
 
     def tearDown(self):
         pass
@@ -50,30 +50,29 @@ class TestLogIn(unittest.TestCase):
 
     # @unittest.skip("")
     def test_get_xsrf(self):
-        _xsrf = login.get_xsrf()
+        _xsrf = self.test_login.get_xsrf()
         self.assertEqual(len(_xsrf), 32, "_xsrf获取失败")
 
     def test_is_login_succeed(self):
         texts = {
             '{"msg": "登陆成功"}': True,
+            '{"msg": "\u767b\u5f55\u6210\u529f"}': True,
             '{"msg": "登录过于频繁，请稍后重试"}': False,
             '{"msg": ""}': False,
             '': False,
         }
         for text in texts:
             print(text, texts[text])
-            self.assertEqual(login.is_login_succeed(text), texts[text], "{text}登录状态判断错误".format(text=text))
+            self.assertEqual(self.test_login.is_login_succeed(text), texts[text], "{text}登录状态判断错误".format(text=text))
 
     def test_get_url_and_postdata(self):
         username, password = login.read_user_info(login.USER_INFO_FILE)
-        url, data = login.get_url_and_postdata(username, password)
+        url, data = self.test_login.get_url_and_postdata(username, password)
         self.assertEqual(type(url), type(""), "数据类型错误")
         self.assertEqual(type(data), type(data), " 数据类型错误")
 
     def test_get_captcha_file(self):
-        import os
-        login.get_captcha_file()
-        self.assertEqual(os.path.exists(login.CAPTCHA_FILE), True, "获取验证码失败")
+        self.assertEqual(self.test_login.get_captcha_file(), True, "获取验证码失败")
 
 
 if __name__ == "__main__":
--- zhihu_scrapy/spiders/login.py
+++ zhihu_scrapy/spiders/login.py
@@ -4,7 +4,7 @@ import requests
 
 USER_INFO_FILE = "user.info"
 COOKIES_FILE = "cookies"
-CAPTCHA_FILE = "captcha.jpg"
+CAPTCHA_FILE = "captcha.bmp"
 IS_VERIFY = True
 
 
@@ -22,16 +22,11 @@ def read_user_info(file):
 def set_headers():
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:46.0) Gecko/20100101 Firefox/46.0",
-        "host": "www.zhihu.com"
+        "host": "www.zhihu.com",
+        'Referer': 'http://www.zhihu.com/',
+        'Accept-Encoding': 'gzip'
     }
     return headers
-
-
-def init_section():
-    session = requests.session()
-    session.headers = set_headers()
-    session.verify = IS_VERIFY
-    return session
 
 
 def is_tel_num(num):
@@ -53,94 +48,100 @@ def _url_select(username):
         return "https://www.zhihu.com/login/email", "email"
 
 
-def is_login_succeed(responst_text):
-    import json
-    try:
-        status = json.loads(responst_text).get("msg")
-    except:
-        return False
-    if status in ["登陆成功"]:
-        return True
-    elif status in ["登录过于频繁，请稍后重试", "email 对应的账户不存在", "验证码会话无效 :("]:
-        return False
-    else:
-        return False
+class Login():
+    def __init__(self):
+        import requests
+        import http.cookiejar
+        self.session = requests.session()
+        self.session.headers = set_headers()
+        self.session.verify = IS_VERIFY
+        self.session.cookies = http.cookiejar.LWPCookieJar(filename=COOKIES_FILE)
 
+    def read_cookie(self, filename=COOKIES_FILE):
+        import os
+        import http.cookiejar
+        self.session.cookies = http.cookiejar.LWPCookieJar(filename=filename)
+        if os.path.exists(filename):
+            self.session.cookies.load()
+            return True
+        else:
+            return False
 
-def get_xsrf():
-    from lxml import etree
-    url = "https://www.zhihu.com/"
-    session = init_section()
-    doc = etree.HTML(session.get(url).text)
-    return doc.xpath('/html/body/input')[0].get("value")
+    def get_xsrf(self):
+        from lxml import etree
+        url = "https://www.zhihu.com/"
+        doc = etree.HTML(self.session.get(url).text)
+        return doc.xpath('/html/body/input')[0].get("value")
 
+    def get_url_and_postdata(self, username, password):
+        url, key = _url_select(username)
+        data = {
+            "email": username,
+            "password": password,
+            "remember_me": True,
+            # "captcha_type": "cn",
+            "_xsrf": self.get_xsrf()
+        }
+        return url, data
 
-def get_url_and_postdata(username, password):
-    url, key = _url_select(username)
-    data = {
-        key: username,
-        "password": password,
-        "remember_me": True,
-        "captcha_type": "cn",
-        "_xsrf": get_xsrf()
-    }
-    return url, data
+    def get_captcha_file(self):
+        import os
+        import time
+        if os.path.exists(CAPTCHA_FILE):
+            os.remove(CAPTCHA_FILE)
+        else:
+            pass
+        t = str(int(time.time() * 1000))
+        captcha_url = 'https://www.zhihu.com/captcha.gif?r' + t + "&type=login"
+        response = self.session.get(url=captcha_url)
+        with open(CAPTCHA_FILE, 'wb') as f:
+            f.write(response.content)
+        if os.path.exists(CAPTCHA_FILE):
+            return True
+        else:
+            return False
 
+    def update_postdata(self, data):
+        import os
+        if self.get_captcha_file():
+            data["captcha"] = input("请打开{captcha_file}，输入验证码：".format(
+                captcha_file=os.path.abspath(CAPTCHA_FILE)
+            ))
+            return data
+        else:
+            return False
 
-def get_captcha_file():
-    import os
-    import time
-    if os.path.exists(CAPTCHA_FILE):
-        os.remove(CAPTCHA_FILE)
-    else:
-        pass
-    t = str(int(time.time() * 1000))
-    session = init_section()
-    captcha_url = 'http://www.zhihu.com/captcha.gif?r' + t + "&type=login"
-    response = session.get(url=captcha_url)
-    with open(CAPTCHA_FILE, 'wb') as f:
-        f.write(response.content)
-    if os.path.exists(CAPTCHA_FILE):
-        return True
-    else:
-        return False
+    @staticmethod
+    def is_login_succeed(responst_text):
+        import json
+        try:
+            status = json.loads(responst_text).get("msg")
+            if status in ["登陆成功", "\u767b\u5f55\u6210\u529f"]:
+                print(status)
+                return True
+            else:
+                print(status)
+                return False
+        except:
+            return False
 
+    def __login(self, username, password):
+        url, data = self.get_url_and_postdata(username, password)
+        response = self.session.post(url=url, data=data)
+        while not self.is_login_succeed(response.text):
+            data = self.update_postdata(data)
+            response = self.session.post(url=url, data=data)
+        self.session.cookies.save()
+        return self.session
 
-def try_to_open_captcha_file(file):
-    try:
-        from PIL import Image
-        Image.open(file)
-    except:
-        pass
-
-
-def update_postdata(data):
-    import os
-    if get_captcha_file():
-        try_to_open_captcha_file(CAPTCHA_FILE)
-        data["captcha"] = input("请打开{captcha_file}，输入验证码".format(
-            captcha_file=os.path.abspath(CAPTCHA_FILE)
-        ))
-        return data
-    else:
-        return False
-
-
-def login(username, password):
-    import json
-    import http.cookiejar
-    session = init_section()
-    session.cookies = http.cookiejar.LWPCookieJar(filename=COOKIES_FILE)
-    url, data = get_url_and_postdata(username, password)
-    response = session.post(url=url, data=json.dumps(data))
-    while not is_login_succeed(response.text):
-        update_postdata(data)
-        response = session.post(url=url, data=json.dumps(data))
-        print(json.loads(response.text).get("msg"))
-    session.cookies.save()
-    return session
+    def login(self, username, password):
+        if self.read_cookie(COOKIES_FILE):
+            return self.session
+        else:
+            print("cookies加载失败，请手动登录！")
+            return self.__login(username, password)
 
 
 if __name__ == '__main__':
     username, password = read_user_info(USER_INFO_FILE)
-    print(login(username, password))
+    session = Login().login(username, password)
